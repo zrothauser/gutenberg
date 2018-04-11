63227170bfd915cb30a8da6009de7950940650b8
Maxime Biais <maxime.biais@gmail.com>
2018-04-11 08:24:09 +0200

    Add iOS stubs for the iOS counter part of the library

---
+++ ios/RNReactNativeRecyclerviewList.m
@@ -0,0 +1,13 @@
+
+#import "RNReactNativeRecyclerviewList.h"
+
+@implementation RNReactNativeRecyclerviewList
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
