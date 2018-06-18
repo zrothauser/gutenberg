fad7919a1e533625d66aa74dcbc7e94c7ca040fa
imath <math.viet@gmail.com>
2018-06-19 00:57:43 +0200

    Update Snapshots and unit tests

--- packages/block-library/src/video/test/index.js
+++ packages/block-library/src/video/test/index.js
@@ -3,9 +3,20 @@
  */
 import { name, settings } from '../';
 import { blockEditRender } from '../../test/helpers';
+import { registerStore } from '@wordpress/data';
 
 describe( 'core/video', () => {
 	test( 'block edit matches snapshot', () => {
+		// The user can upload files.
+		registerStore( 'core', {
+			reducer( state = { hasUploadPermissions: true } ) {
+				return state;
+			},
+			selectors: {
+				hasUploadPermissions: ( state ) => state.hasUploadPermissions,
+			},
+		} );
+
 		const wrapper = blockEditRender( name, settings );
 
 		expect( wrapper ).toMatchSnapshot();
