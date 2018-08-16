c52d2bbdc39d63634d7fc8b4398950f7d877efff
Daniel Richards <daniel.richards@automattic.com>
2018-08-16 18:30:32 +0800

    Remove unneeded dependencies from MediaUploadCheck and simplify logic

--- packages/editor/src/components/media-upload/check.js
+++ packages/editor/src/components/media-upload/check.js
@@ -1,22 +1,15 @@
 /**
  * WordPress dependencies
  */
-import { Fragment } from '@wordpress/element';
-import { compose } from '@wordpress/compose';
 import { withSelect } from '@wordpress/data';
 
 export function MediaUploadCheck( { hasUploadPermissions, fallback, children } ) {
-	if ( ! hasUploadPermissions ) {
-		return fallback || null;
-	}
-
-	return <Fragment>{ children }</Fragment>;
+	const optionalFallback = fallback || null;
+	return hasUploadPermissions ? children : optionalFallback;
 }
 
-export default compose( [
-	withSelect( ( select ) => {
-		return {
-			hasUploadPermissions: select( 'core' ).hasUploadPermissions(),
-		};
-	} ),
-] )( MediaUploadCheck );
+export default withSelect( ( select ) => {
+	return {
+		hasUploadPermissions: select( 'core' ).hasUploadPermissions(),
+	};
+} )( MediaUploadCheck );
