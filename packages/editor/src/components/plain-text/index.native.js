d583c63af62de1c6d225688a4f356f0df6d58f88
Mario Zorz <mariozorz@gmail.com>
2018-11-20 22:22:38 -0300

    hack: width change artifact to force update on TextInput and make text selectable

--- packages/editor/src/components/plain-text/index.native.js
+++ packages/editor/src/components/plain-text/index.native.js
@@ -14,10 +14,24 @@ import { Component } from '@wordpress/element';
 import styles from './style.scss';
 
 export default class PlainText extends Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+		  // Initial width must be different so React makes the update
+		  clutchWidth: '99%'
+		}
+	}
+
 	componentDidMount() {
 		// if isSelected is true, we should request the focus on this TextInput
 		if ( ( this._input.isFocused() === false ) && ( this._input.props.isSelected === true ) ) {
 			this.focus();
+		}
+
+		if ( this.state.clutchWidth !== '100%') {
+			setTimeout(() => {
+				this.setState( { clutchWidth: '100%'} )
+			}, 100)
 		}
 	}
 
@@ -28,10 +42,12 @@ export default class PlainText extends Component {
 	render() {
 		return (
 			<TextInput
+				{ ...this.props }
+				style={ [ this.props.style, { width: this.state.clutchWidth } ] }
+				removeClippedSubviews={ false }
 				ref={ ( x ) => this._input = x }
 				className={ [ styles[ 'editor-plain-text' ], this.props.className ] }
 				onChangeText={ ( text ) => this.props.onChange( text ) }
-				{ ...this.props }
 			/>
 		);
 	}
