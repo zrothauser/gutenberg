4992edd151c30864eb66b497ff741897d35341e7
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2019-01-04 18:32:39 +0200

    First stab at enabling media-text block. Redscreens.

--- packages/block-library/src/index.native.js
+++ packages/block-library/src/index.native.js
@@ -11,6 +11,7 @@ import {
  */
 import * as code from './code';
 import * as heading from './heading';
+import * as mediaText from './media-text';
 import * as more from './more';
 import * as paragraph from './paragraph';
 import * as image from './image';
@@ -21,6 +22,7 @@ export const registerCoreBlocks = () => {
 		paragraph,
 		heading,
 		code,
+		mediaText,
 		more,
 		image,
 		nextpage,
--- packages/editor/src/components/index.native.js
+++ packages/editor/src/components/index.native.js
@@ -1,5 +1,6 @@
 export * from './colors';
 export * from './font-sizes';
+export { default as InnerBlocks } from './inner-blocks';
 export { default as PlainText } from './plain-text';
 export { default as RichText } from './rich-text';
 export { default as MediaPlaceholder } from './media-placeholder';
