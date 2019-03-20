54833ac2c3058fb8a299c634e568dce496ff54b7
Tugdual de Kerviler <dekervit@gmail.com>
2019-03-20 16:26:52 +0100

    Refactor mobile blocks to fix circle dependencies

--- packages/block-library/src/image/edit.native.js
+++ packages/block-library/src/image/edit.native.js
@@ -27,15 +27,12 @@ import {
 	RichText,
 	BlockControls,
 	InspectorControls,
-} from '@wordpress/block-editor';
-import {
 	BottomSheet,
 	Picker,
-} from '@wordpress/editor';
+} from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { isURL } from '@wordpress/url';
 import { doAction, hasAction } from '@wordpress/hooks';
-
 
 /**
  * Internal dependencies
@@ -91,7 +88,7 @@ class ImageEdit extends React.Component {
 	componentWillUnmount() {
 		// this action will only exist if the user pressed the trash button on the block holder
 		if ( hasAction( 'blocks.onRemoveBlockCheckUpload' ) && this.state.isUploadInProgress ) {
-			doAction( 'blocks.onRemoveBlockCheckUpload', this.props.attributes.id )
+			doAction( 'blocks.onRemoveBlockCheckUpload', this.props.attributes.id );
 		}
 		this.removeMediaUploadListener();
 	}
