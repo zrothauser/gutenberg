2f9ce49418f2289ccd49f3bc9a0a7b23a7ab20d0
Andrei Draganescu <me@andreidraganescu.info>
2019-04-05 09:26:01 +0300

    adds not working proof of concept

--- packages/block-library/src/code/edit.js
+++ packages/block-library/src/code/edit.js
@@ -1,22 +1,112 @@
 /**
  * WordPress dependencies
  */
+import {
+	Toolbar,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import {
+	Component,
+} from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import { PlainText } from '@wordpress/block-editor';
+import { BlockControls, PlainText } from '@wordpress/block-editor';
 
-export default function CodeEdit( { attributes, setAttributes, className } ) {
-	return (
-		<div className={ className }>
-			<PlainText
-				value={ attributes.content }
-				onChange={ ( content ) => setAttributes( { content } ) }
-				placeholder={ __( 'Write code…' ) }
-				aria-label={ __( 'Code' ) }
-			/>
-		</div>
-	);
+class CodeEdit extends Component {
+	constructor( props ) {
+		super( props );
+		const {
+			attributes,
+		} = props;
+		this.state = {
+			content: attributes.content,
+		};
+		this.layoutControls = [
+			{
+				icon: 'arrow-right-alt',
+				title: __( 'Indent code' ),
+				onClick: () => this.indentCode(),
+			},
+			{
+				icon: 'arrow-left-alt',
+				title: __( 'Unindent code' ),
+				onClick: () => this.unIndentCode(),
+			},
+		];
+	}
+
+	getIndentation( content ) {
+		const lines = content.split( '\n' );
+		return lines.reduce( ( line, indent ) => {
+			if ( line.startsWith( '\t' ) ) {
+				indent = 'tab';
+			}
+			if ( line.startsWith( ' ' ) ) {
+				indent = 'space';
+			}
+			return indent;
+		} );
+	}
+
+	indentCode() {
+		const identation = this.getIndentation( this.state.content );
+		const splitContent = this.state.content.split( '\n' );
+		const newContent = splitContent.map( ( line ) => {
+			if ( identation === 'tab' ) {
+				return `\t${ line }`;
+			}
+			if ( identation === 'space' ) {
+				return `  ${ line }`;
+			}
+			return line;
+		} ).join( '\n' );
+
+		this.setState( {
+			content: newContent,
+		} );
+	}
+
+	unIndentCode() {
+		const identation = this.getIndentation( this.state.content );
+		const splitContent = this.state.content.split( '\n' );
+		const newContent = splitContent.map( ( line ) => {
+			if ( identation === 'tab' ) {
+				return `${ line }`.replace( '\t', '' );
+			}
+			if ( identation === 'space' ) {
+				return line.replace( '  ', '' );
+			}
+			return line;
+		} ).join( '\n' );
+
+		this.setState( {
+			content: newContent,
+		} );
+	}
+
+	render() {
+		const {
+			className,
+		} = this.props;
+
+		return (
+			<div className={ className }>
+				<BlockControls>
+					<Toolbar controls={ this.layoutControls } />
+				</BlockControls>
+				<PlainText
+					value={ this.state.content }
+					onChange={ ( content ) => {
+						this.setState( { content } );
+					} }
+					placeholder={ __( 'Write code…' ) }
+					aria-label={ __( 'Code' ) }
+				/>
+			</div>
+		);
+	}
 }
+
+export default CodeEdit;
