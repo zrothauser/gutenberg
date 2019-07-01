c36ec9a8ae8b0cc17db15ca2256365af00ec80ed
Marcus Kazmierczak <marcus@mkaz.com>
2019-07-01 13:20:11 -0700

    Add timezone to PostSchedule date
    Fixes #15673
    
    Removes the settings format and replaces with a date format that
    includes the timezone. The setting inside the date format package is
    equally hard-coded and not from wp-admin settings configuration.
    
    It makes sense to me to hard-code the setting closer to the usage, I'm
    open to alternatives, issue #15673 highlights a couple of different
    solutions considered but did not receive any feedback.

--- packages/editor/src/components/post-schedule/label.js
+++ packages/editor/src/components/post-schedule/label.js
@@ -2,14 +2,14 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { dateI18n, __experimentalGetSettings } from '@wordpress/date';
+import { dateI18n } from '@wordpress/date';
 import { withSelect } from '@wordpress/data';
 
 export function PostScheduleLabel( { date, isFloating } ) {
-	const settings = __experimentalGetSettings();
+	const formatWithTimezone = 'M j, Y g: i a T';
 
 	return date && ! isFloating ?
-		dateI18n( settings.formats.datetimeAbbreviated, date ) :
+		dateI18n( formatWithTimezone, date ) :
 		__( 'Immediately' );
 }
 
