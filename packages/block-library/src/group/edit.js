958f5d26f8cc6a275dfcc962eeca3488a4b363a3
Dave Smith <getdavemail@gmail.com>
2019-07-24 21:15:37 +0100

    Add Responsive Controls and Attributes

--- packages/block-library/src/group/edit.js
+++ packages/block-library/src/group/edit.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import classnames from 'classnames';
+import { partialRight } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -16,9 +17,13 @@ import {
 	withColors,
 	DimensionControl,
 } from '@wordpress/block-editor';
+import {
+	Fragment,
+} from '@wordpress/element';
 
 import {
 	PanelBody,
+	ToggleControl,
 } from '@wordpress/components';
 
 function GroupEdit( {
@@ -34,16 +39,43 @@ function GroupEdit( {
 		backgroundColor: backgroundColor.color,
 	};
 
-	const hasPadding = attributes.paddingSize !== '';
-	const hasMargin = attributes.marginSize !== '';
+	const hasPadding = ( device = '' ) => !! attributes[ `paddingSize${ device }` ];
+	const hasMargin = ( device = '' ) => !! attributes[ `marginSize${ device }` ];
 
 	const classes = classnames( className, backgroundColor.class, {
 		'has-background': !! backgroundColor.color,
-		'has-padding': hasPadding,
-		'has-margin': hasMargin,
-		[ `padding-${ attributes.paddingSize }` ]: hasPadding,
-		[ `margin-${ attributes.marginSize }` ]: hasMargin,
+		'has-padding': hasPadding(),
+		'has-margin': hasMargin(),
+		'has-responsive-padding': attributes.responsivePadding,
+		'has-responsive-margin': attributes.responsiveMargin,
+		[ `padding-${ attributes.paddingSize }` ]: hasPadding(),
+		[ `padding-tablet-${ attributes.paddingSizeTablet }` ]: attributes.responsivePadding && hasPadding( 'Tablet' ),
+		[ `padding-mobile-${ attributes.paddingSizeMobile }` ]: attributes.responsivePadding && hasPadding( 'Mobile' ),
+		[ `margin-${ attributes.marginSize }` ]: hasMargin(),
+		[ `margin-tablet-${ attributes.marginSizeTablet }` ]: hasMargin( 'Tablet' ),
+		[ `margin-mobile-${ attributes.marginSizeMobile }` ]: hasMargin( 'Mobile' ),
 	} );
+
+	const resetSpacingDimension = ( dimension, device = '' ) => {
+		setAttributes( {
+			[ `${ dimension }${ device }` ]: '',
+		} );
+	};
+
+	const resetResponsiveSpacing = () => {
+		setAttributes( {
+			paddingSizeMobile: '',
+			paddingSizeTablet: '',
+			marginSizeMobile: '',
+			marginSizeTablet: '',
+		} );
+	};
+
+	const updateSpacing = ( dimension, size, device = '' ) => {
+		setAttributes( {
+			[ `${ dimension }${ device }` ]: size,
+		} );
+	};
 
 	return (
 		<>
@@ -60,20 +92,143 @@ function GroupEdit( {
 				/>
 
 				<PanelBody title={ __( 'Spacing' ) }>
-					<DimensionControl
-						title={ __( 'Padding' ) }
-						property="padding"
-						attributes={ attributes }
-						setAttributes={ setAttributes }
-						clientId={ clientId }
-					/>
-					<DimensionControl
-						title={ __( 'Margin' ) }
-						property="margin"
-						attributes={ attributes }
-						setAttributes={ setAttributes }
-						clientId={ clientId }
-					/>
+
+					<fieldset className="block-editor-responsive-controls">
+						<legend className="block-editor-responsive-controls__label">{ __( 'Padding' ) } </legend>
+						<ToggleControl
+							label={ __( 'Manually adjust Padding based on screensize?' ) }
+							checked={ attributes.responsivePadding }
+							onChange={ () => {
+								const responsivePadding = ! attributes.responsivePadding;
+
+								if ( ! responsivePadding ) {
+									resetResponsiveSpacing();
+								}
+								setAttributes( {
+									responsivePadding,
+								} );
+							} }
+						/>
+
+						{ ! attributes.responsivePadding && (
+							<DimensionControl
+								title={ __( 'Padding' ) }
+								property="padding"
+								clientId={ clientId }
+								onReset={ resetSpacingDimension }
+								onSpacingChange={ updateSpacing }
+								currentSize={ attributes.paddingSize }
+								device="all"
+								deviceIcon="desktop"
+							/>
+						) }
+
+						{ attributes.responsivePadding && (
+							<Fragment>
+								<DimensionControl
+									title={ __( 'Padding' ) }
+									property="padding"
+									clientId={ clientId }
+									onReset={ resetSpacingDimension }
+									onSpacingChange={ updateSpacing }
+									currentSize={ attributes.paddingSize }
+									device="desktop"
+								/>
+
+								<DimensionControl
+									title={ __( 'Padding' ) }
+									property="padding"
+									clientId={ clientId }
+									onReset={ partialRight( resetSpacingDimension, 'Tablet' ) }
+									onSpacingChange={ partialRight( updateSpacing, 'Tablet' ) }
+									currentSize={ attributes.paddingSizeTablet }
+									device="tablet"
+									deviceIcon="tablet"
+								/>
+
+								<DimensionControl
+									title={ __( 'Padding' ) }
+									property="padding"
+									clientId={ clientId }
+									onReset={ partialRight( resetSpacingDimension, 'Mobile' ) }
+									onSpacingChange={ partialRight( updateSpacing, 'Mobile' ) }
+									currentSize={ attributes.paddingSizeMobile }
+									device="mobile"
+									deviceIcon="smartphone"
+								/>
+
+							</Fragment>
+						) }
+					</fieldset>
+
+					<fieldset className="block-editor-responsive-controls">
+						<legend className="block-editor-responsive-controls__label">{ __( 'Margin' ) }</legend>
+						<ToggleControl
+							label={ __( 'Manually adjust Margin based on screensize?' ) }
+							checked={ attributes.responsiveMargin }
+							onChange={ () => {
+								const responsiveMargin = ! attributes.responsiveMargin;
+
+								if ( ! responsiveMargin ) {
+									resetResponsiveSpacing();
+								}
+								setAttributes( {
+									responsiveMargin,
+								} );
+							} }
+						/>
+
+						{ ! attributes.responsiveMargin && (
+							<DimensionControl
+								title={ __( 'Margin' ) }
+								property="padding"
+								clientId={ clientId }
+								onReset={ resetSpacingDimension }
+								onSpacingChange={ updateSpacing }
+								currentSize={ attributes.marginSize }
+								device="all"
+								deviceIcon="desktop"
+							/>
+						) }
+
+						{ attributes.responsiveMargin && (
+							<Fragment>
+								<DimensionControl
+									title={ __( 'Margin' ) }
+									property="margin"
+									clientId={ clientId }
+									onReset={ resetSpacingDimension }
+									onSpacingChange={ updateSpacing }
+									currentSize={ attributes.marginSize }
+									device="desktop"
+								/>
+
+								<DimensionControl
+									title={ __( 'Margin' ) }
+									property="margin"
+									clientId={ clientId }
+									onReset={ partialRight( resetSpacingDimension, 'Tablet' ) }
+									onSpacingChange={ partialRight( updateSpacing, 'Tablet' ) }
+									currentSize={ attributes.marginSizeTablet }
+									device="tablet"
+									deviceIcon="tablet"
+								/>
+
+								<DimensionControl
+									title={ __( 'Margin' ) }
+									property="margin"
+									clientId={ clientId }
+									onReset={ partialRight( resetSpacingDimension, 'Mobile' ) }
+									onSpacingChange={ partialRight( updateSpacing, 'Mobile' ) }
+									currentSize={ attributes.marginSizeMobile }
+									device="mobile"
+									deviceIcon="smartphone"
+								/>
+
+							</Fragment>
+						) }
+					</fieldset>
+
 				</PanelBody>
 			</InspectorControls>
 			<div className={ classes } style={ styles }>
