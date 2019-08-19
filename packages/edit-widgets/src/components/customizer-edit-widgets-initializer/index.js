7d7e1cc2fd03915f5f019c5cc6d4fdf9b9ab927f
Jorge <jorge.costa@developer.pt>
2019-08-19 13:34:08 +0100

    Add mechanism to set a width on withViewportMatch

--- packages/edit-widgets/src/components/customizer-edit-widgets-initializer/index.js
+++ packages/edit-widgets/src/components/customizer-edit-widgets-initializer/index.js
@@ -7,6 +7,7 @@ import {
 	navigateRegions,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { ViewportMatchWidthProvider } from '@wordpress/viewport';
 
 /**
  * Internal dependencies
@@ -15,19 +16,23 @@ import WidgetAreas from '../widget-areas';
 
 import './sync-customizer';
 
+const CUSTOMIZER_PANEL_WIDTH = 275;
+
 function CustomizerEditWidgetsInitializer( { settings } ) {
 	return (
-		<SlotFillProvider>
-			<div
-				className="edit-widgets-customizer-edit-widgets-initializer__content"
-				role="region"
-				aria-label={ __( 'Widgets screen content' ) }
-				tabIndex="-1"
-			>
-				<WidgetAreas blockEditorSettings={ settings } />
-			</div>
-			<Popover.Slot className="edit-widgets-popover-slot" />
-		</SlotFillProvider>
+		<ViewportMatchWidthProvider width={ CUSTOMIZER_PANEL_WIDTH }>
+			<SlotFillProvider>
+				<div
+					className="edit-widgets-customizer-edit-widgets-initializer__content"
+					role="region"
+					aria-label={ __( 'Widgets screen content' ) }
+					tabIndex="-1"
+				>
+					<WidgetAreas blockEditorSettings={ settings } />
+				</div>
+				<Popover.Slot className="edit-widgets-popover-slot" />
+			</SlotFillProvider>
+		</ViewportMatchWidthProvider>
 	);
 }
 
--- packages/viewport/src/index.js
+++ packages/viewport/src/index.js
@@ -13,34 +13,10 @@ import { dispatch } from '@wordpress/data';
  */
 import './store';
 
+import { BREAKPOINTS, OPERATORS } from './with-viewport-match';
+
 export { default as ifViewportMatches } from './if-viewport-matches';
-export { default as withViewportMatch } from './with-viewport-match';
-
-/**
- * Hash of breakpoint names with pixel width at which it becomes effective.
- *
- * @see _breakpoints.scss
- *
- * @type {Object}
- */
-const BREAKPOINTS = {
-	huge: 1440,
-	wide: 1280,
-	large: 960,
-	medium: 782,
-	small: 600,
-	mobile: 480,
-};
-
-/**
- * Hash of query operators with corresponding condition for media query.
- *
- * @type {Object}
- */
-const OPERATORS = {
-	'<': 'max-width',
-	'>=': 'min-width',
-};
+export { default as withViewportMatch, ViewportMatchWidthProvider } from './with-viewport-match';
 
 /**
  * Callback invoked when media query state should be updated. Is invoked a
