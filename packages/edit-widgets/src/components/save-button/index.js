0a6cbee45e567ad56393800290dbd290cc886138
Jorge <jorge.costa@developer.pt>
2019-09-03 22:25:39 +0100

    WIP: use entities on the widget screen.

--- packages/edit-widgets/src/components/save-button/index.js
+++ packages/edit-widgets/src/components/save-button/index.js
@@ -1,19 +1,45 @@
 /**
+ * External dependencies
+ */
+import { filter, map, some, forEach } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { useDispatch } from '@wordpress/data';
-import { useState, useCallback } from '@wordpress/element';
+import { useCallback } from '@wordpress/element';
+import { useDispatch, useSelect } from '@wordpress/data';
 
 function SaveButton() {
-	const [ isSaving, setIsSaving ] = useState( false );
-	const { saveWidgetAreas } = useDispatch( 'core/edit-widgets' );
-	const onClick = useCallback( async () => {
-		setIsSaving( true );
-		await saveWidgetAreas();
-		setIsSaving( false );
-	}, [] );
+	const { widgetAreaEdits, isSaving } = useSelect(
+		( select ) => {
+			const {
+				hasEditsForEntityRecord,
+				isSavingEntityRecord,
+				getEntityRecords,
+			} = select( 'core' );
+			const widgetAreas = getEntityRecords( 'root', 'widgetArea' );
+			const widgetAreaIds = map( widgetAreas, ( { id } ) => id );
+			return {
+				widgetAreaEdits: filter(
+					widgetAreaIds,
+					( id ) => hasEditsForEntityRecord( 'root', 'widgetArea', id )
+				),
+				isSaving: some(
+					widgetAreaIds,
+					( id ) => isSavingEntityRecord( 'root', 'widgetArea', id )
+				),
+			};
+		}
+	);
+	const { saveEditedEntityRecord } = useDispatch( 'core' );
+	//const { saveWidgetAreas } = useDispatch( 'core/edit-widgets' );
+	const onClick = useCallback( () => {
+		forEach( widgetAreaEdits, ( id ) => {
+			saveEditedEntityRecord( 'root', 'widgetArea', id );
+		} );
+	}, [ widgetAreaEdits ] );
 
 	return (
 		<Button
@@ -22,6 +48,7 @@ function SaveButton() {
 			isBusy={ isSaving }
 			aria-disabled={ isSaving }
 			onClick={ isSaving ? undefined : onClick }
+			disabled={ widgetAreaEdits.length === 0 }
 		>
 			{ __( 'Update' ) }
 		</Button>
