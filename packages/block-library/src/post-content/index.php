1723b8f148ca5e8096b6f36207ea613681e4daa8
Felix Arntz <felixarntz@google.com>
2019-09-18 13:54:36 +0200

    Detect current post more reliably in core/post-title and core/post-content renderers.

--- packages/block-library/src/post-content/index.php
+++ packages/block-library/src/post-content/index.php
@@ -8,17 +8,21 @@
 /**
  * Renders the `core/post-content` block on the server.
  *
+ * @global WP_Query $wp_query WordPress query object.
+ *
  * @return string Returns the filtered post content of the current post.
  */
 function render_block_core_post_content() {
-	// TODO: Without this temporary fix, an infinite loop can occur.
-	if ( is_admin() || defined( 'REST_REQUEST' ) ) {
-		return '';
-	}
+	global $wp_query;
 
-	if ( ! in_the_loop() ) {
+	if ( isset( $wp_query ) && ! in_the_loop() ) {
 		rewind_posts();
 		the_post();
+	}
+
+	// Ensure no infinite loop occurs.
+	if ( 'wp_template' === get_post_type() ) {
+		return '';
 	}
 
 	return '<div class="entry-content">' . apply_filters( 'the_content', str_replace( ']]>', ']]&gt;', get_the_content() ) ) . '</div>';
--- packages/block-library/src/post-title/index.php
+++ packages/block-library/src/post-title/index.php
@@ -8,12 +8,21 @@
 /**
  * Renders the `core/post-title` block on the server.
  *
+ * @global WP_Query $wp_query WordPress query object.
+ *
  * @return string Returns the filtered post title for the current post wrapped inside "h1" tags.
  */
 function render_block_core_post_title() {
-	if ( ! in_the_loop() ) {
+	global $wp_query;
+
+	if ( isset( $wp_query ) && ! in_the_loop() ) {
 		rewind_posts();
 		the_post();
+	}
+
+	// Ensure no infinite loop occurs.
+	if ( 'wp_template' === get_post_type() ) {
+		return '';
 	}
 
 	return the_title( '<h1>', '</h1>', false );
